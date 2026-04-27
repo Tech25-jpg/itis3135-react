762946cf325f605057d16a17327ce444d6fc6565
Tech25-jpg <kteah25@gmail.com>
2026-04-26 23:36:22 -0400

    UPD 11 | css / slideshow test 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Slideshow from './components/Slideshow';
+import Slideshow from './pages/Slideshow';
 
 const Home = () => (
   <div>
